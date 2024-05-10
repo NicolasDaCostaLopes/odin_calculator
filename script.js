04ad859efee4c037294a16de1c4f30db202a7d25
Nicolas D - WSL2 <135014303+NicolasDaCostaLopes@users.noreply.github.com>
2024-05-10 23:38:49 +0200

    Implemented some logic

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let first;
 let operator;
 let second;
 const calculator = document.querySelector(".calculator");
+const displayValue = document.querySelector(".value")
 const add = (a, b) => {
     return a + b
 }
@@ -18,19 +19,53 @@ const divide = (a, b) => {
     return a / b
 }
 
-const operate = (a, b, operation) => {
-    switch (operation) {
+const operate = () => {
+    let result
+    switch (operator) {
         case "add":
-            return add(a, b);
+            result = add(first, second);
         case "substract":
-            return substract(a, b);
+            result = substract(first, second);
         case "multiply":
-            return multiply(a, b);
+            result = multiply(first, second);
         case "divide":
-            return divide(a, b);
+            result = divide(first, second);
+    }
+    [first, second, operator] = [second, null, null]
+    changeDisplayValue(result)
+    return result
+}
+const appendDisplayValue = (value) => {
+    if (displayValue.textContent.length <= 14) {
+        if (displayValue.textContent == 0) {
+            updateStoredValues(displayValue.textContent = value)
+            return
+        }
+        updateStoredValues(displayValue.textContent += value)
     }
 }
-const eventHandler = (e) => {
-    console.log(e.target.value);
+const changeDisplayValue = (value) => {
+    displayValue.textContent = value
 }
-calculator.addEventListener("click", eventHandler);
+const updateStoredValues = (value) => {
+    operator ? second = value : first = value
+    console.log(`first : ${first}`)
+    console.log(`operator : ${operator}`)
+    console.log(`second : ${second}`)
+    console.log("\n");
+
+}
+const eventHandlerButtons = (e) => {
+    if (isNaN(+e.target.value) && e.target.value) {
+        if (e.target.value === "add" || e.target.value === "substract" || e.target.value === "multiply" || e.target.value === "divide") {
+            operator = e.target.value
+            changeDisplayValue(0);
+            console.log(operator);
+        }
+    }
+    else if (typeof +e.target.value === "number" && !isNaN(+e.target.value)) {
+        appendDisplayValue(e.target.value);
+    }
+
+}
+calculator.addEventListener("click", eventHandlerButtons);
