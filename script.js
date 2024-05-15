d1d5d01255349d5a48fe6c06c60dded79f86b73b
Nicolas D - WSL2 <135014303+NicolasDaCostaLopes@users.noreply.github.com>
2024-05-15 20:13:11 +0200

    Implemented equals and limited length number o n display

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let first;
 let operator;
 let second;
+const maxNumberSize = 14;
 const calculator = document.querySelector(".calculator");
 const displayValue = document.querySelector(".value")
 const add = (a, b) => {
@@ -23,20 +24,27 @@ const operate = () => {
     let result
     switch (operator) {
         case "add":
-            result = add(first, second);
+            result = add(+first, +second);
+            break
         case "substract":
             result = substract(first, second);
+            break
         case "multiply":
             result = multiply(first, second);
+            break
         case "divide":
             result = divide(first, second);
+            break
     }
-    [first, second, operator] = [second, null, null]
-    changeDisplayValue(result)
+    updateStoredValues(result, true);
     return result
 }
 const appendDisplayValue = (value) => {
-    if (displayValue.textContent.length <= 14) {
+    if (operator && displayValue.textContent == first) {
+        updateStoredValues(displayValue.textContent = value)
+        return
+    }
+    if (displayValue.textContent.length <= maxNumberSize) {
         if (displayValue.textContent == 0) {
             updateStoredValues(displayValue.textContent = value)
             return
@@ -45,9 +53,24 @@ const appendDisplayValue = (value) => {
     }
 }
 const changeDisplayValue = (value) => {
-    displayValue.textContent = value
+    if ((value % 1)) {
+        displayValue.textContent = numberDecimalLength(value);
+        // Loss of precision here, i don't like it
+        first = numberDecimalLength(value);
+    } else {
+        displayValue.textContent = value
+    }
 }
-const updateStoredValues = (value) => {
+const numberDecimalLength = (value) => {
+    let length = maxNumberSize - Math.abs(Math.trunc((value / 1)).toString().length);
+    return value.toFixed(length);
+}
+const updateStoredValues = (value, afterOperate) => {
+    if (afterOperate) {
+        [first, second, operator] = [value, null, null]
+        changeDisplayValue(value);
+        return
+    }
     operator ? second = value : first = value
     console.log(`first : ${first}`)
     console.log(`operator : ${operator}`)
@@ -55,15 +78,28 @@ const updateStoredValues = (value) => {
     console.log("\n");
 
 }
+const clearCalculator = () => {
+    [first, second, operator] = [0, null, null]
+    changeDisplayValue(first);
+}
+
 const eventHandlerButtons = (e) => {
     if (isNaN(+e.target.value) && e.target.value) {
         if (e.target.value === "add" || e.target.value === "substract" || e.target.value === "multiply" || e.target.value === "divide") {
+            if (operator) {
+                operate();
+            }
             operator = e.target.value
-            changeDisplayValue(0);
-            console.log(operator);
+        }
+        if (e.target.value === "equal") {
+            operate();
+        }
+        if (e.target.value === "clear") {
+            clearCalculator();
         }
     }
     else if (typeof +e.target.value === "number" && !isNaN(+e.target.value)) {
+        console.log("clicked")
         appendDisplayValue(e.target.value);
     }
 
